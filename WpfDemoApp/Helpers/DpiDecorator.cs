6c07352348032ca57a766eda7576d38a8598ac2e
Trong Huu <tronghuu379@gmail.com>
2023-07-18 00:42:11 +0900

    add window style

---
+++ WpfDemoApp/Helpers/DpiDecorator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfDemoApp.Helpers
+{
+    public class DpiDecorator : Decorator
+    {
+        public DpiDecorator()
+        {
+            Loaded += (s, e) =>
+            {
+                Matrix m = PresentationSource.FromVisual(this).CompositionTarget.TransformToDevice;
+                ScaleTransform dpiTransform = new ScaleTransform(1 / m.M11, 1 / m.M22);
+                if (dpiTransform.CanFreeze)
+                    dpiTransform.Freeze();
+                LayoutTransform = dpiTransform;
+                DpiTransform = dpiTransform;
+            };
+        }
+
+        public static Transform DpiTransform { get; private set; }
+    }
+}
